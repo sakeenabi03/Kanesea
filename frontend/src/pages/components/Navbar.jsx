ac8b1d795fdd923cf1fbfb1436b79458935b84ed
sakeena bi <sakeenabi03@gmail.com>
2026-06-16 19:32:29 +0530

    Additional changes

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -237,10 +237,10 @@ function Navbar({ isFixed = true }) {
                                     <MdAccountCircle size={35} />
                                     <div className={`absolute right-0 mt-2 w-max px-1 font-bold bg-white border border-gray-200 rounded-lg shadow-2xl py-2 z-[999] transform transition-all duration-300 ease-out origin-top cursor-pointer ${ accountLoginMenuOpen ? "opacity-100 scale-100 -translate-x-5 translate-y-0" : "opacity-0 scale-0 translate-x-15 translate-y-0 pointer-events-none" }`}>
                                         <p className="px-4 py-2 text-black text-lg border-b cursor-default" >
-                                            Welcome {userData.fullName.split(" ")[0]}
+                                            Welcome {userData?.fullName?.split(" ")[0]}
                                         </p>
                                         <p className="px-4 py-2 text-gray-700 text-lg hover:text-black flex items-center group gap-2" onClick={() =>  { navigate("/profile"); setAccountLoginMenuOpen(false); }} >
-                                            <span className='bg-gray-700 text-white py-[2px] px-[10px] rounded-full group-hover:bg-black'> {userData.fullName.slice(0,1)} </span> 
+                                            <span className='bg-gray-700 text-white py-[2px] px-[10px] rounded-full group-hover:bg-black'> {userData?.fullName?.slice(0,1)} </span> 
                                             Profile
                                         </p>
                                         <p className="px-4 py-2 text-gray-700 text-lg hover:text-black flex items-center gap-2" onClick={() => { navigate("/my-orders"); setAccountLoginMenuOpen(false); }} >
